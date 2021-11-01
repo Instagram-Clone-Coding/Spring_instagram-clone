4c0dce1bdc6f9dd847b64e9e13bc1cff1fff4aa1
DH CHOI <bluetifulc@gmail.com>
2021-11-01 18:05:49 +0900

    Feat: Member 엔티티 개선
    userid가 누락되어있어서 추가
    ImageType을 Enum을 만들어서 관리
    Column 명을 명시하도록 변경
    생성자를 AllArgsConstructor에서 필요한 정보만 받도록 수정

---
+++ src/main/java/cloneproject/Instagram/entity/member/ImageType.java
@@ -0,0 +1,5 @@
+package cloneproject.Instagram.entity.member;
+
+public enum ImageType {
+    PNG, JPG, JPEG;
+}
--- src/main/java/cloneproject/Instagram/entity/member/Member.java
+++ src/main/java/cloneproject/Instagram/entity/member/Member.java
@@ -3,51 +3,72 @@ package cloneproject.Instagram.entity.member;
 import javax.persistence.*;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@Builder
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "member")
+@Table(name = "members")
 public class Member {
 
     @Id
+    @Column(name = "member_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 20, unique = true)
+    @Column(name = "member_userid", nullable = false, length = 20, unique = true)
+    private String userid;
+
+    @Column(name = "member_username", nullable = false, length = 20)
     private String username;
 
-    @Column(nullable = false)
+    @Column(name = "member_password", nullable = false)
     private String password;
 
+    @Column(name = "member_website")
     private String website;
 
     @Lob
+    @Column(name = "member_introduce")
     private String introduce;
 
+    @Column(name = "member_email")
     private String email;
 
+    @Column(name = "member_phone")
     private String phone;
 
+    @Column(name = "member_gender")
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
-    @Column(name = "image_url")
+    @Column(name = "member_image_url")
     private String imageUrl;
 
-    @Column(name = "image_type")
-    private String imageType;
+    @Column(name = "member_image_type")
+    @Enumerated(EnumType.STRING)
+    private ImageType imageType;
 
-    @Column(name = "image_name")
+    @Column(name = "member_image_name")
     private String imageName;
 
-    @Column(name = "image_uuid")
+    @Column(name = "member_image_uuid")
     private String imageUUID;
 
+    @Builder
+    public Member(String userid, String username, String password, String phone){
+        this.userid = userid;
+        this.username = username;
+        this.password = password;
+        this.phone = phone;
+        
+        // 자동 초기화
+        this.gender = Gender.PRIVATE;
+        this.imageUrl = "https://drive.google.com/file/d/1Gu0DcGCJNs4Vo0bz2U9U6v01d_VwKijs/view?usp=sharing";
+        this.imageName = "BaseImageName";
+        this.imageType = ImageType.PNG;
+        this.imageUUID = "BaseImageUUID";
+    }
 }
