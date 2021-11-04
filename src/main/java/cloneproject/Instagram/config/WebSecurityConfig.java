8fedf6cc40e8588d4e4521ff3b239f9fd5eb6dbb
DH CHOI <bluetifulc@gmail.com>
2021-11-04 17:29:05 +0900

    Fix: Security config에 Bean추가
    BCryptPasswordEncoder Bean 추가

--- src/main/java/cloneproject/Instagram/config/WebSecurityConfig.java
+++ src/main/java/cloneproject/Instagram/config/WebSecurityConfig.java
@@ -1,11 +1,13 @@
 package cloneproject.Instagram.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @Configuration
@@ -13,6 +15,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     public void configure(WebSecurity web) throws Exception{
         web.ignoring().antMatchers("/css/**", "/js/**"); // 나중에 수정
