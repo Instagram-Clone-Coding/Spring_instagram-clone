63f8a89216e5dac8177ef18bc5f234c37f68f7dc
DH CHOI <bluetifulc@gmail.com>
2021-11-04 23:08:30 +0900

    Fix: RegisterRequest에 정규표현식검증 추가
    RegisterRequest의 password, phone에 정규표현식 검증 추가

--- src/main/java/cloneproject/Instagram/dto/RegisterRequest.java
+++ src/main/java/cloneproject/Instagram/dto/RegisterRequest.java
@@ -1,7 +1,7 @@
 package cloneproject.Instagram.dto;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 import cloneproject.Instagram.entity.member.Member;
 import lombok.AllArgsConstructor;
@@ -9,7 +9,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-// TODO valdiation
 @Getter
 @Setter
 @NoArgsConstructor
@@ -23,9 +22,12 @@ public class RegisterRequest {
     private String username;
 
     @NotBlank(message = "비밀번호를 입력해주세요")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", 
+                message = "비밀번호는 8자 이상, 최소 하나의 문자와 숫자가 필요합니다")
     private String password;
 
     @NotBlank(message = "휴대폰 번호를 입력해주세요")
+    @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "휴대폰 번호 양식이 맞지 않습니다")
     private String phone;
     
     public Member convert(){
