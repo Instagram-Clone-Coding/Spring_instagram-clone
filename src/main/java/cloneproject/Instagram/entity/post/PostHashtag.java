fd12653c94fd4fd613971b35b065f8fb17a2b649
seonpilKim <ksp970306@gmail.com>
2021-11-07 15:16:42 +0900

    Feat: Hashtag 엔티티 추가
    - CommentHashtag
    - Hashtag
    - HighlightStoryHashtag
    - PostHashtag
    - StoryHashtag
    
    Resolve: #13

---
+++ src/main/java/cloneproject/Instagram/entity/post/PostHashtag.java
@@ -0,0 +1,37 @@
+package cloneproject.Instagram.entity.post;
+
+import cloneproject.Instagram.entity.hashtag.Hashtag;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Table(name = "post_hashtags")
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostHashtag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_hashtag_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hashtag_id")
+    private Hashtag hashtag;
+
+    @Builder
+    public PostHashtag(Post post, Hashtag hashtag) {
+        this.post = post;
+        this.hashtag = hashtag;
+    }
+}
