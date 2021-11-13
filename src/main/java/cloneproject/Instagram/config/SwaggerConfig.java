2b3cd8e059c76a63e30402ca3b1b4c802c1193fc
seonpilKim <ksp970306@gmail.com>
2021-11-13 22:16:57 +0900

    Feat: Swagger 설정 추가
    Resolve: #30

---
+++ src/main/java/cloneproject/Instagram/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package cloneproject.Instagram.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("cloneproject.Instagram.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Instagram's API Docs")
+                .version("1.0")
+                .description("API 명세서")
+                .build();
+    }
+}
