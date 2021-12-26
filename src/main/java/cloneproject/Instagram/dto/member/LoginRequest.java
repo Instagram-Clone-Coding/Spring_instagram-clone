647e6c743fbce9ca32e0cfa162fe0122c9bbb14b
DH CHOI <bluetifulc@gmail.com>
2021-12-26 19:51:06 +0900

    Feat: Member, Follow 관련 Swagger 설정 추가

--- src/main/java/cloneproject/Instagram/dto/member/LoginRequest.java
+++ src/main/java/cloneproject/Instagram/dto/member/LoginRequest.java
@@ -4,6 +4,7 @@ import javax.validation.constraints.NotBlank;
 
 import org.hibernate.validator.constraints.Length;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,10 +16,12 @@ import lombok.Setter;
 @AllArgsConstructor
 public class LoginRequest {
     
+    @ApiModelProperty(value = "유저네임", example = "dlwlrma", required = true)
     @NotBlank(message = "ID를 입력해주세요")
     @Length(min = 4, max = 12, message = "ID는 4문자 이상 12문자 이하여야 합니다")
     private String username;
 
+    @ApiModelProperty(value = "비밀번호", example = "a12341234", required = true)
     @NotBlank(message = "비밀번호를 입력해주세요")
     @Length(max = 20, message = "비밀번호는 20문자 이하여야 합니다")
     private String password;
--- src/main/java/cloneproject/Instagram/dto/member/ReissueRequest.java
+++ src/main/java/cloneproject/Instagram/dto/member/ReissueRequest.java
@@ -2,7 +2,7 @@ package cloneproject.Instagram.dto.member;
 
 import javax.validation.constraints.NotBlank;
 
-
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,9 +14,11 @@ import lombok.NoArgsConstructor;
 @Builder
 public class ReissueRequest {
 
+    @ApiModelProperty(value = "Access 토큰", example = "AAA.BBB.CCC", required = true)
     @NotBlank(message = "유효한 Access Token이 없습니다")
     private String accessToken;
 
+    @ApiModelProperty(value = "Refresh 토큰", example = "AAA.BBB.CCC", required = true)
     @NotBlank(message = "유효한 Refresh Token이 없습니다")
     private String refreshToken;
 
