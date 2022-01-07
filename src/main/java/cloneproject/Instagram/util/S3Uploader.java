008b87c0a1fcb71caaa8c2436e4eb77b8a2c6628
DH CHOI <bluetifulc@gmail.com>
2022-01-07 22:37:47 +0900

    Feat: AWS 파일 업로드 관련 설정
    gradle 의존성 추가
    AWS config과 S3 버킷 업로드를 위한 유틸 클래스 구현

---
+++ src/main/java/cloneproject/Instagram/util/S3Uploader.java
@@ -0,0 +1,103 @@
+package cloneproject.Instagram.util;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import org.hibernate.type.descriptor.java.MutabilityPlan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import cloneproject.Instagram.exception.CantConvertFileException;
+import cloneproject.Instagram.vo.Image;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class S3Uploader {
+    
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    public String bucket;
+
+    public String upload(MultipartFile multipartFile, String dirName, String UUID, String name, String type) throws IOException {
+        File uploadFile = convert(multipartFile) 
+                .orElseThrow(CantConvertFileException::new);
+
+        return upload(uploadFile, dirName, UUID, name, type);
+    }
+
+    public Image uploadImage(MultipartFile multipartFile, String dirName) throws IOException{
+        Image image = ImageUtil.convertMultipartToImage(multipartFile);
+        String url = upload(multipartFile, dirName, image.getImageUUID(),
+                                     image.getImageName(), image.getImageType().toString());
+        image.setUrl(url);
+        return image;
+    }
+
+    public void delete(String dirName, String UUID, String name){
+        String filename = dirName + "/" + UUID + "_" + name;
+        deleteS3(filename);
+    }
+
+    public void deleteImage(String dirName, Image image){
+        if(image.getImageUUID().equals("base-UUID")){
+            return;
+        }
+        String filename = dirName + "/" + image.getImageUUID() + "_" + image.getImageName() 
+                                                + "." + image.getImageType().toString();
+        deleteS3(filename);
+    }
+
+
+    private String upload(File uploadFile, String dirName, String UUID, String name, String type) {
+        String fileName = dirName + "/" + UUID + "_" + name + "." + type;
+        String uploadImageUrl = putS3(uploadFile, fileName);
+        log.info(uploadImageUrl);
+        removeNewFile(uploadFile);
+        return uploadImageUrl;
+    }
+
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    private void deleteS3(String filename){
+        amazonS3Client.deleteObject(bucket, filename);
+    }
+
+    private void removeNewFile(File targetFile) {
+        if (targetFile.delete()) {
+            log.info("File delete success");
+            return;
+        }
+        log.info("File delete fail");
+    }
+
+    // 로컬에 파일 업로드 하기
+    private Optional<File> convert(MultipartFile file) throws IOException {
+        File convertFile = new File(System.getProperty("user.dir") + "\\upload\\" + file.getOriginalFilename());
+        log.info(convertFile.getAbsolutePath());
+        if (convertFile.createNewFile()) { // 바로 위에서 지정한 경로에 File이 생성됨 (경로가 잘못되었다면 생성 불가능)
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) { // FileOutputStream 데이터를 파일에 바이트 스트림으로 저장하기 위함
+                fos.write(file.getBytes());
+            }
+            return Optional.of(convertFile);
+        }
+
+        return Optional.empty();
+    }
+
+
+}
