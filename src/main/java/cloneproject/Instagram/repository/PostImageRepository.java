d197d78c3eed19a40dd4ed9e6b02923524c17f01
seonpilKim <ksp970306@gmail.com>
2022-01-17 00:55:51 +0900

    Etc: 불필요 파일 제거

--- src/main/java/cloneproject/Instagram/repository/PostImageRepository.java
+++ src/main/java/cloneproject/Instagram/repository/PostImageRepository.java
@@ -3,5 +3,8 @@ package cloneproject.Instagram.repository;
 import cloneproject.Instagram.entity.post.PostImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostImageRepository extends JpaRepository<PostImage, Long> {
+    List<PostImage> findAllByPostId(Long postId);
 }
