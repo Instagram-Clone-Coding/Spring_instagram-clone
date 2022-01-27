fc0e48ed3892a693ac6a908f2d5e798970ece5c2
DH CHOI <bluetifulc@gmail.com>
2022-01-27 19:38:37 +0900

    Refactor: 댓글 알림 관련 보류사항
    댓글의 처리 로직이 복잡해 보류하기로 결정

--- src/main/java/cloneproject/Instagram/dto/alarm/AlarmType.java
+++ src/main/java/cloneproject/Instagram/dto/alarm/AlarmType.java
@@ -8,8 +8,8 @@ import lombok.Getter;
 public enum AlarmType {
     
     POST_LIKES_ALARM("agentUsername이 targetUsername(나)의 postId인 포스트를 좋아합니다", "postId"),
-    POST_COMMENT_ALARM("agentUsername이 targetUsername(나)의 postId인 포스트에 댓글을 남겼습니다.", "commentId"),
-    COMMENT_LIKES_ALARM("agentUsername이 targetUsername(나)의 postId인 포스트에 댓글을 남겼습니다.", "commentId"),
+    POST_COMMENT_ALARM("agentUsername이 targetUsername(나)의 postId인 포스트에 댓글을 남겼습니다.", "postId"),
+    COMMENT_LIKES_ALARM("agentUsername이 targetUsername(나)의 postId인 포스트에 댓글을 남겼습니다.", "postId"),
     MEMBER_FOLLOW_ALARM("agentUsername이 targetUsername(나)를 팔로우 합니다", "followId"),
     MEMBER_TAGGED_ALARM("agentUsername이 postId인 포스트에서 targetUsername(나)를 태그했습니다", "postId");
 
--- src/main/java/cloneproject/Instagram/repository/AlarmRepositoryQuerydslImpl.java
+++ src/main/java/cloneproject/Instagram/repository/AlarmRepositoryQuerydslImpl.java
@@ -1,18 +1,11 @@
 package cloneproject.Instagram.repository;
 
 import java.util.List;
-import java.util.Map;
-
-import com.querydsl.core.group.GroupBy;
-import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import cloneproject.Instagram.dto.alarm.AlarmDTO;
-import cloneproject.Instagram.dto.alarm.AlarmType;
 import cloneproject.Instagram.dto.alarm.QAlarmDTO;
 import lombok.RequiredArgsConstructor;
-
-import static cloneproject.Instagram.entity.comment.QComment.comment;
 import static cloneproject.Instagram.entity.alarms.QAlarm.alarm;
 
 @RequiredArgsConstructor
@@ -23,28 +16,30 @@ public class AlarmRepositoryQuerydslImpl implements AlarmRepositoryQuerydsl{
     @Override
     public List<AlarmDTO> getAlarms(Long loginedMemberId){
 
-        Map<Long, AlarmDTO> resultAboutComment = queryFactory
-                                    .from(alarm)
-                                    .where(alarm.target.id.eq(loginedMemberId).and(alarm.type.eq(AlarmType.POST_COMMENT_ALARM)))
-                                    .transform(GroupBy.groupBy(alarm.id).as(new QAlarmDTO(
-                                        alarm.id,
-                                        alarm.type, 
-                                        alarm.agent.username, 
-                                        alarm.target.username, 
-                                        alarm.itemId, 
-                                        alarm.createdAt)));
 
-        Map<Long, Long> postIds = queryFactory
-                                    .from(alarm)
-                                    .where(alarm.target.id.eq(loginedMemberId).and(alarm.type.eq(AlarmType.POST_COMMENT_ALARM)))
-                                    .transform(GroupBy.groupBy(alarm.id).as(
-                                        JPAExpressions
-                                            .select(comment.post.id)
-                                            .from(comment)
-                                            .where(comment.id.eq(alarm.itemId))        
-                                    ));
+        // * 주석 처리한 부분은 로직이 복잡해 보류됨 참고 : Issue #76
+        // Map<Long, AlarmDTO> resultAboutComment = queryFactory
+        //                             .from(alarm)
+        //                             .where(alarm.target.id.eq(loginedMemberId).and(alarm.type.eq(AlarmType.POST_COMMENT_ALARM)))
+        //                             .transform(GroupBy.groupBy(alarm.id).as(new QAlarmDTO(
+        //                                 alarm.id,
+        //                                 alarm.type, 
+        //                                 alarm.agent.username, 
+        //                                 alarm.target.username, 
+        //                                 alarm.itemId, 
+        //                                 alarm.createdAt)));
+
+        // Map<Long, Long> postIds = queryFactory
+        //                             .from(alarm)
+        //                             .where(alarm.target.id.eq(loginedMemberId).and(alarm.type.eq(AlarmType.POST_COMMENT_ALARM)))
+        //                             .transform(GroupBy.groupBy(alarm.id).as(
+        //                                 JPAExpressions
+        //                                     .select(comment.post.id)
+        //                                     .from(comment)
+        //                                     .where(comment.id.eq(alarm.itemId))        
+        //                             ));
         
-        resultAboutComment.forEach((key, alarm)->alarm.getItemIds().put("postId", postIds.get(key)));
+        // resultAboutComment.forEach((key, alarm)->alarm.getItemIds().put("postId", postIds.get(key)));
 
         List<AlarmDTO> result = queryFactory
                                     .select(new QAlarmDTO(
@@ -56,10 +51,11 @@ public class AlarmRepositoryQuerydslImpl implements AlarmRepositoryQuerydsl{
                                         alarm.createdAt))
 
                                     .from(alarm)
-                                    .where(alarm.target.id.eq(loginedMemberId).and(alarm.type.ne(AlarmType.POST_COMMENT_ALARM)))
+                                    .where(alarm.target.id.eq(loginedMemberId))
+                                    // .where(alarm.target.id.eq(loginedMemberId).and(alarm.type.ne(AlarmType.POST_COMMENT_ALARM)))
                                     .fetch();
 
-        result.addAll(resultAboutComment.values()); // ? 최적화 방법 떠오르지않음
+        // result.addAll(resultAboutComment.values());
         return result;
     }
 
