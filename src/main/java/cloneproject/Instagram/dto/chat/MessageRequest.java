e8de33045ca9bc2af69245d80ef3a2b905582231
seonpilKim <ksp970306@gmail.com>
2022-02-02 20:13:34 +0900

    Feat: 채팅 메시지 전송 기능 추가
    Resolve: #81

---
+++ src/main/java/cloneproject/Instagram/dto/chat/MessageRequest.java
@@ -0,0 +1,25 @@
+package cloneproject.Instagram.dto.chat;
+
+import cloneproject.Instagram.entity.chat.MessageType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor
+public class MessageRequest {
+
+    @NotNull(message = "채팅방 PK는 필수입니다.")
+    private Long roomId;
+
+    @NotNull(message = "메시지를 보내는 회원 PK는 필수입니다.")
+    private Long senderId;
+
+    @NotEmpty(message = "채팅 메시지는 필수입니다.")
+    private String content;
+
+    @NotNull(message = "채팅 메시지 타입은 필수입니다.")
+    private MessageType messageType;
+}
