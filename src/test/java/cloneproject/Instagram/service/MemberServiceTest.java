49da79b23cbf3323b9546c1b7a505ad3ae1c29b8
DH CHOI <bluetifulc@gmail.com>
2022-02-09 17:41:53 +0900

    Fix: Service Test 어노테이션 수정
    불필요한 빈을 모두 로딩하는 어노테이션 수정

--- src/test/java/cloneproject/Instagram/service/MemberServiceTest.java
+++ src/test/java/cloneproject/Instagram/service/MemberServiceTest.java
@@ -10,9 +10,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import cloneproject.Instagram.WithMockCustomUser;
 import cloneproject.Instagram.dto.member.EditProfileRequest;
@@ -25,7 +27,7 @@ import cloneproject.Instagram.repository.EmailCodeRedisRepository;
 import cloneproject.Instagram.repository.MemberRepository;
 import cloneproject.Instagram.util.ImageUtil;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 @DisplayName("Member Service")
 public class MemberServiceTest {
     
