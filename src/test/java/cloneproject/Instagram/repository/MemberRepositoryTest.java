16872b025ea03c734191033cfbb209daf413ebfc
DH CHOI <bluetifulc@gmail.com>
2022-02-12 13:46:14 +0900

    Test: MemberRepository 테스트
    MemberRepository 테스트 작성
     - JPA에서 지원하는 메서드는 제외하고 querydsl등을 이용해 직접 짠 로직만 테스트

--- src/test/java/cloneproject/Instagram/repository/MemberRepositoryTest.java
+++ src/test/java/cloneproject/Instagram/repository/MemberRepositoryTest.java
@@ -1,63 +1,188 @@
 package cloneproject.Instagram.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.TestPropertySource;
 
+import cloneproject.Instagram.dto.member.MiniProfileResponse;
+import cloneproject.Instagram.dto.member.SearchedMemberDTO;
+import cloneproject.Instagram.dto.member.UserProfileResponse;
+import cloneproject.Instagram.entity.member.Follow;
 import cloneproject.Instagram.entity.member.Member;
 
 @DataJpaTest
-@ExtendWith(SpringExtension.class)
-@AutoConfigureTestDatabase(replace = Replace.NONE)
 @DisplayName("Member Repository")
+@TestPropertySource("classpath:application-test.yml")
 public class MemberRepositoryTest {
     
 
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private FollowRepository followRepository;
+
+    private Member savedMemberOne;
+    private Member savedMemberTwo;
+    private Member savedMemberThree;
+    private static Long loginedId;
     
-    @Nested
-    @DisplayName("save 메서드는")
-    class Describe_save{
+    @BeforeEach
+    void prepare(){
+        memberRepository.deleteAll();
+        final Member givenMemberOne =  Member.builder()
+                                        .username("dlwlrma")
+                                        .name("이지금")
+                                        .password("1234")
+                                        .email("123@gmail.com")
+                                        .build();
+        final Member givenMemberTwo =  Member.builder()
+                                        .username("dlwlrma1")
+                                        .name("이지금")
+                                        .password("1234")
+                                        .email("123@gmail.com")
+                                        .build();
+        final Member givenMemberThree =  Member.builder()
+                                        .username("dlwldms")
+                                        .name("이지은")
+                                        .password("1234")
+                                        .email("123@gmail.com")
+                                        .build();
+        savedMemberOne = memberRepository.save(givenMemberOne);
+        savedMemberTwo = memberRepository.save(givenMemberTwo);
+        savedMemberThree = memberRepository.save(givenMemberThree);
 
-        // TODO querydsl 테스트 추가
-        @BeforeEach
-        void prepare(){
-            memberRepository.deleteAll();
-        }
-
-        @Nested
-        @DisplayName("멤버 객체가 주어지면")
-        class Context_given_member{
-            final Member givenMember =  Member.builder()
-                                            .username("testusername")
-                                            .name("testname")
-                                            .password("1234")
-                                            .email("123@gmail.com")
-                                            .build();
-            
-            @Test
-            @DisplayName("주어진 멤버를 저장한 뒤, 저장된 멤버를 반환한다.")
-            void it_save_member_and_return_saved_member(){
-                assertNull(givenMember.getId(), "저장전 id는 없어야 한다");
-                final Member savedMember = memberRepository.save(givenMember);
-                
-                assertNotNull(savedMember.getId(), "저장후 id는 존재해야 한다");
-                assertEquals(savedMember.getUsername(), givenMember.getUsername(), "저장된 정보가 일치해야 한다");
-            }
-        }
+        Follow follow = Follow.builder()
+                                .member(savedMemberOne)
+                                .followMember(savedMemberTwo)
+                                .build();
+        followRepository.save(follow);
+        follow = Follow.builder()
+                                .member(savedMemberThree)
+                                .followMember(savedMemberOne)
+                                .build();
+        followRepository.save(follow);
+        follow = Follow.builder()
+                                .member(savedMemberOne)
+                                .followMember(savedMemberThree)
+                                .build();
+        followRepository.save(follow);
+        follow = Follow.builder()
+                                .member(savedMemberThree)
+                                .followMember(savedMemberTwo)
+                                .build();
+        followRepository.save(follow);
+        loginedId = savedMemberOne.getId();
 
     }
+
+    @Nested
+    @DisplayName("getUserProfile 메서드는")
+    class Describe_getUserProfile{
+        @Nested
+        @DisplayName("로그인한 상태로 요청하면")
+        class Context_logined{
+            @Test
+            @DisplayName("정상적으로 결과를 반환한다")
+            void it_returns_well(){
+                UserProfileResponse userProfileResponse = memberRepository.getUserProfile(loginedId, savedMemberTwo.getUsername());
+                assertEquals(savedMemberTwo.getUsername(), userProfileResponse.getMemberUsername());
+                assertEquals(savedMemberTwo.getName(), userProfileResponse.getMemberName());
+                assertEquals(2L, userProfileResponse.getMemberFollowersCount());
+                assertEquals(savedMemberThree.getUsername(), userProfileResponse.getFollowingMemberFollow());
+                assertTrue(userProfileResponse.isFollowing());
+            }
+        }
+        @Nested
+        @DisplayName("로그인하지 않은 상태로 요청하면")
+        class Context_unlogined{
+            @Test
+            @DisplayName("정상적으로 결과를 반환하나, 특정 결과가 고정된다")
+            void it_returns_well(){
+                UserProfileResponse userProfileResponse = memberRepository.getUserProfile(-1L, savedMemberTwo.getUsername());
+                assertEquals(savedMemberTwo.getUsername(), userProfileResponse.getMemberUsername());
+                assertEquals(savedMemberTwo.getName(), userProfileResponse.getMemberName());
+                assertEquals(2L, userProfileResponse.getMemberFollowersCount());
+                assertNull(userProfileResponse.getFollowingMemberFollow());
+                assertFalse(userProfileResponse.isFollowing());
+            }
+        }
+    }
+    @Nested
+    @DisplayName("getMiniProfile 메서드는")
+    class Describe_getMiniProfile{
+        @Nested
+        @DisplayName("로그인한 상태로 요청하면")
+        class Context_logined{
+            @Test
+            @DisplayName("정상적으로 결과를 반환한다")
+            void it_returns_well(){
+                MiniProfileResponse miniProfileResponse = memberRepository.getMiniProfile(loginedId, savedMemberTwo.getUsername());
+                assertEquals(savedMemberTwo.getUsername(), miniProfileResponse.getMemberUsername());
+                assertEquals(savedMemberTwo.getName(), miniProfileResponse.getMemberName());
+                assertEquals(2L, miniProfileResponse.getMemberFollowersCount());
+                assertEquals(savedMemberThree.getUsername(), miniProfileResponse.getFollowingMemberFollow());
+                assertTrue(miniProfileResponse.isFollowing());
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("searchMember 메서드는")
+    class Describe_searchMember{
+        @Nested
+        @DisplayName("dlwl를 검색하면")
+        class Context_dlwl{
+            @Test
+            @DisplayName("3명의 유저가 검색된다")
+            void it_returns_three_users(){
+                List<SearchedMemberDTO> searchedMemberDTOs = memberRepository.searchMember(loginedId, "dlwl");
+                assertEquals(3, searchedMemberDTOs.size());
+                assertEquals(savedMemberOne.getUsername(), searchedMemberDTOs.get(0).getUsername());
+                assertEquals(savedMemberThree.getUsername(), searchedMemberDTOs.get(1).getFollowingMemberFollow().get(0).getMemberUsername());
+            }
+        }
+        @Nested
+        @DisplayName("dlwlrma를 검색하면")
+        class Context_dlwlrma{
+            @Test
+            @DisplayName("2명의 유저가 검색된다")
+            void it_returns_two_users(){
+                List<SearchedMemberDTO> searchedMemberDTOs = memberRepository.searchMember(loginedId, "dlwlrma");
+                assertEquals(2, searchedMemberDTOs.size());
+                assertEquals(savedMemberOne.getUsername(), searchedMemberDTOs.get(0).getUsername());
+                assertEquals(savedMemberThree.getUsername(), searchedMemberDTOs.get(1).getFollowingMemberFollow().get(0).getMemberUsername());
+            }
+        }
+    }
+    @Nested
+    @DisplayName("findAllByUsernames 메서드는")
+    class Describe_findAllByUsernames{
+        @Nested
+        @DisplayName("2개의 username을 넘기면")
+        class Context_two_usernames{
+            @Test
+            @DisplayName("2명의 유저가 리턴된다")
+            void it_returns_two_users(){
+                List<String> usernameList = new ArrayList<>();
+                usernameList.add(savedMemberOne.getUsername());
+                usernameList.add(savedMemberTwo.getUsername());
+                List<Member> members = memberRepository.findAllByUsernames(usernameList);
+                assertEquals(2, members.size());
+            }
+        }
+    }
+
     
 }
