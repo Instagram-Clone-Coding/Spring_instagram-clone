d969e297f05569a3a443cf8e37c849009dbbe69d
DH CHOI <bluetifulc@gmail.com>
2022-03-07 20:23:43 +0900

    Refactor: 회원가입 인증코드 수정
    - 회원가입 인증코드를 전송하는 메일에 HTML 적용
    - HTML을 전송하기 위해 EmailService 로직 변경
    - 이메일 관련 예외처리 추가

--- src/main/java/cloneproject/Instagram/service/EmailService.java
+++ src/main/java/cloneproject/Instagram/service/EmailService.java
@@ -1,10 +1,14 @@
 package cloneproject.Instagram.service;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import cloneproject.Instagram.exception.CantSendEmailException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -16,6 +20,20 @@ public class EmailService {
     @Async
     public void sendEmail(SimpleMailMessage email){
         javaMailSender.send(email);
+    }
+
+    @Async
+    public void sendHtmlTextEmail(String subject, String content, String email){
+        MimeMessage message = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+            messageHelper.setTo(email);
+            messageHelper.setSubject(subject);
+            messageHelper.setText(content, true);
+            javaMailSender.send(message);
+        }catch(Exception e){
+            throw new CantSendEmailException();
+        }
     }
 
 }
