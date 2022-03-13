752e7fb3731017052b30b6199a510c372fb35897
seonpilKim <ksp970306@gmail.com>
2022-03-14 01:21:45 +0900

    Etc: 불필요 파일 제거

--- src/main/java/cloneproject/Instagram/repository/story/StoryContentRepository.java
+++
@@ -1,7 +0,0 @@
-package cloneproject.Instagram.repository.story;
-
-import cloneproject.Instagram.entity.story.StoryContent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoryContentRepository extends JpaRepository<StoryContent, Long> {
-}
