a6a145b24653d085af1426c17fe14b8c42ebdfdc
seonpilKim <ksp970306@gmail.com>
2022-03-14 19:56:35 +0900

    Feat: 스토리 업로드 요청 데이터 수정
    - 스토리 이미지에 추가하는 텍스트들은 기존 이미지 위에 합친 이미지로 서버에 전달하는 방식
    - 따라서 각 텍스트의 태그 위치를 서버에 저장할 필요가 없음
    - 멘션 기능을 위해 content는 유지
    
    Resolve: #130

--- src/main/java/cloneproject/Instagram/dto/story/StoryContentRequest.java
+++ src/main/java/cloneproject/Instagram/dto/story/StoryContentRequest.java
@@ -1,21 +1,17 @@
 package cloneproject.Instagram.dto.story;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class StoryContentRequest {
 
     @ApiModelProperty(value = "스토리 이미지 순번", example = "1", required = true)
-    private Long id;
-
-    @ApiModelProperty(value = "스토리 텍스트 x좌표", example = "50", required = true)
-    private Long x;
-
-    @ApiModelProperty(value = "스토리 텍스트 y좌표", example = "50", required = true)
-    private Long y;
+    private Integer id;
 
     @ApiModelProperty(value = "스토리 텍스트 내용", example = "This is a story @dlwlrma", required = true)
     private String content;
