08b7683394e1f22305bd1731d5328c2635b13e2a
DH CHOI <bluetifulc@gmail.com>
2022-03-20 15:26:36 +0900

    Fix: 기존의 유저 검색 API 제거

--- src/main/java/cloneproject/Instagram/repository/MemberRepositoryQuerydsl.java
+++ src/main/java/cloneproject/Instagram/repository/MemberRepositoryQuerydsl.java
@@ -4,7 +4,6 @@ package cloneproject.Instagram.repository;
 import java.util.List;
 
 import cloneproject.Instagram.dto.member.MiniProfileResponse;
-import cloneproject.Instagram.dto.member.SearchedMemberDTO;
 import cloneproject.Instagram.dto.member.UserProfileResponse;
 import cloneproject.Instagram.entity.member.Member;
 
@@ -12,7 +11,6 @@ public interface MemberRepositoryQuerydsl {
     
     UserProfileResponse getUserProfile(Long loginedUserId, String username);
     MiniProfileResponse getMiniProfile(Long loginedUserId, String username);
-    List<SearchedMemberDTO> searchMember(Long loginedUserId, String text);
     List<Member> findAllByUsernames(List<String> usernames);
 
 }
