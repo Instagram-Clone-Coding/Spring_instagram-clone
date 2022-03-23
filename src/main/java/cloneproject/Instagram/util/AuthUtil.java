cc599528749ef7af89be537932f5b6362175c75c
seonpilKim <ksp970306@gmail.com>
2022-03-24 06:04:06 +0900

    Feat: AuthUtil 메소드 추가

--- src/main/java/cloneproject/Instagram/util/AuthUtil.java
+++ src/main/java/cloneproject/Instagram/util/AuthUtil.java
@@ -1,16 +1,33 @@
 package cloneproject.Instagram.util;
 
+import cloneproject.Instagram.entity.member.Member;
+import cloneproject.Instagram.exception.MemberDoesNotExistException;
+import cloneproject.Instagram.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class AuthUtil {
-    
-    public static Long getLoginedMemberIdOrNull(){
-        try{
+
+    private final MemberRepository memberRepository;
+
+    public static Long getLoginedMemberIdOrNull() {
+        try {
             final String memberId = SecurityContextHolder.getContext().getAuthentication().getName();
             return Long.valueOf(memberId);
-        }catch(Exception e){
+        } catch (Exception e) {
             return -1L;
         }
     }
 
+    public Member getLoginMember() {
+        try {
+            final Long memberId = Long.valueOf(SecurityContextHolder.getContext().getAuthentication().getName());
+            return memberRepository.findById(memberId).orElseThrow(MemberDoesNotExistException::new);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
 }
