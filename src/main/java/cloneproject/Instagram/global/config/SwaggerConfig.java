71cf1cf9a3154d6e475239154364067483ec57ca
seonpilKim <ksp970306@gmail.com>
2022-05-09 19:48:14 +0900

    Feat: Swagger 설정 추가
    - Authorization 헤더 required = false로 변경
    - globalResponseMessage 추가
    
    Resolve: #161

--- src/main/java/cloneproject/Instagram/global/config/SwaggerConfig.java
+++ src/main/java/cloneproject/Instagram/global/config/SwaggerConfig.java
@@ -2,14 +2,18 @@ package cloneproject.Instagram.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,34 +25,44 @@ import java.util.List;
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public Docket api() {
-        // Authorization header 글로벌 처리
-        List global = new ArrayList();
-        global.add(new ParameterBuilder()
-                .name("Authorization")
-                .description("Access Token")
-                .parameterType("header")
-                .required(true)
-                .modelRef(new ModelRef("string"))
-                .scalarExample("Bearer AAA.BBB.CCC")
-                .build());
+	@Bean
+	public Docket api() {
+		// Authorization header 글로벌 처리
+		final List<Parameter> global = new ArrayList<>();
+		global.add(new ParameterBuilder()
+			.name("Authorization")
+			.description("Access Token")
+			.parameterType("header")
+			.required(false)
+			.modelRef(new ModelRef("string"))
+			.scalarExample("Bearer AAA.BBB.CCC")
+			.build());
 
-        return new Docket(DocumentationType.SWAGGER_2)
-                .globalOperationParameters(global)
-                .useDefaultResponseMessages(false)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
-                .paths(PathSelectors.any())
-                .build();
-    }
+		final List<ResponseMessage> responseMessages = new ArrayList<>();
+		responseMessages.add(new ResponseMessageBuilder().code(405)
+			.message("G002 - 허용되지 않은 HTTP method입니다.").build());
+		responseMessages.add(new ResponseMessageBuilder().code(500)
+			.message("G001 - 내부 서버 오류입니다.").build());
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Instagram's API Docs")
-                .version("1.0")
-                .description("API 명세서")
-                .build();
-    }
+		return new Docket(DocumentationType.SWAGGER_2)
+			.globalOperationParameters(global)
+			.useDefaultResponseMessages(false)
+			.globalResponseMessage(RequestMethod.POST, responseMessages)
+			.globalResponseMessage(RequestMethod.GET, responseMessages)
+			.globalResponseMessage(RequestMethod.DELETE, responseMessages)
+			.globalResponseMessage(RequestMethod.PUT, responseMessages)
+			.apiInfo(apiInfo())
+			.select()
+			.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+			.paths(PathSelectors.any())
+			.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+			.title("Instagram's API Docs")
+			.version("1.0")
+			.description("API 명세서")
+			.build();
+	}
 }
