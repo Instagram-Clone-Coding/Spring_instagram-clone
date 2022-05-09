997c7ce17c6c88552830207400fc3ebc5c414014
DH CHOI <bluetifulc@gmail.com>
2022-05-10 00:53:04 +0900

    Refactor: Member 도메인 Controller 리팩토링 - final 적용할 수 있는 부분에 적용 - return 형식 변경: result 객체를 따로 만드는 방식에서 ResponseEntity의 ok함수를 이용하는 방식으로 변경 - Bean Validation   - Controller class에 @Validated 추가   - 의미없는 @Validated 삭제   - DTO의 검증은 @Valid로 통일 - 중복되는 코드 메서드로 분리(refreshToken 쿠키 생성) - 저장된 게시물 API의 endpoint 변경(의미 없는 username 삭제)

--- src/main/java/cloneproject/Instagram/domain/member/controller/BlockController.java
+++ src/main/java/cloneproject/Instagram/domain/member/controller/BlockController.java
@@ -1,8 +1,7 @@
 package cloneproject.Instagram.domain.member.controller;
 
-import javax.validation.constraints.NotBlank;
+import static cloneproject.Instagram.global.result.ResultCode.*;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cloneproject.Instagram.domain.member.service.BlockService;
-import cloneproject.Instagram.global.result.ResultCode;
 import cloneproject.Instagram.global.result.ResultResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -21,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 @Api(tags = "차단 API")
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class BlockController {
 
 	private final BlockService blockService;
@@ -28,23 +27,19 @@ public class BlockController {
 	@ApiOperation(value = "차단")
 	@PostMapping("/{blockMemberUsername}/block")
 	@ApiImplicitParam(name = "blockMemberUsername", value = "차단할 계정의 username", required = true, example = "dlwlrma")
-	public ResponseEntity<ResultResponse> block(
-			@PathVariable("blockMemberUsername") @Validated @NotBlank(message = "username이 필요합니다") String blockMemberUsername) {
-		boolean success = blockService.block(blockMemberUsername);
+	public ResponseEntity<ResultResponse> block(@PathVariable("blockMemberUsername") String blockMemberUsername) {
+		final boolean success = blockService.block(blockMemberUsername);
 
-		ResultResponse result = ResultResponse.of(ResultCode.BLOCK_SUCCESS, success);
-		return new ResponseEntity<>(result, HttpStatus.valueOf(result.getStatus()));
+		return ResponseEntity.ok(ResultResponse.of(BLOCK_SUCCESS, success));
 	}
 
 	@ApiOperation(value = "차단해제")
 	@DeleteMapping("/{blockMemberUsername}/block")
 	@ApiImplicitParam(name = "blockMemberUsername", value = "차단해제할 계정의 username", required = true, example = "dlwlrma")
-	public ResponseEntity<ResultResponse> unblock(
-			@PathVariable("blockMemberUsername") @Validated @NotBlank(message = "username이 필요합니다") String blockMemberUsername) {
-		boolean success = blockService.unblock(blockMemberUsername);
+	public ResponseEntity<ResultResponse> unblock(@PathVariable("blockMemberUsername") String blockMemberUsername) {
+		final boolean success = blockService.unblock(blockMemberUsername);
 
-		ResultResponse result = ResultResponse.of(ResultCode.UNBLOCK_SUCCESS, success);
-		return new ResponseEntity<>(result, HttpStatus.valueOf(result.getStatus()));
+		return ResponseEntity.ok(ResultResponse.of(UNBLOCK_SUCCESS, success));
 	}
 
 }
--- src/main/java/cloneproject/Instagram/domain/member/controller/MemberPostController.java
+++ src/main/java/cloneproject/Instagram/domain/member/controller/MemberPostController.java
@@ -2,9 +2,6 @@ package cloneproject.Instagram.domain.member.controller;
 
 import java.util.List;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -26,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 @Api(tags = "멤버 게시물 API")
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class MemberPostController {
 
 	private final MemberPostService memberPostService;
@@ -36,8 +34,7 @@ public class MemberPostController {
 		@ApiImplicitParam(name = "username", value = "유저네임", required = true, example = "dlwlrma")
 	})
 	@GetMapping("/accounts/{username}/posts/recent")
-	public ResponseEntity<ResultResponse> getRecent10Posts(
-		@PathVariable("username") @Validated @NotBlank(message = "username은 필수입니다") String username) {
+	public ResponseEntity<ResultResponse> getRecent10Posts(@PathVariable("username") String username) {
 		final List<MemberPostDTO> postList = memberPostService.getRecent15PostDTOs(username);
 
 		return ResponseEntity.ok(ResultResponse.of(ResultCode.FIND_RECENT15_MEMBER_POSTS_SUCCESS, postList));
@@ -50,9 +47,8 @@ public class MemberPostController {
 		@ApiImplicitParam(name = "page", value = "페이지", required = true, example = "1")
 	})
 	@GetMapping("/accounts/{username}/posts")
-	public ResponseEntity<ResultResponse> getPostPage(
-		@PathVariable("username") @Validated @NotBlank(message = "username은 필수입니다") String username,
-		@Validated @NotNull(message = "조회할 게시물 page는 필수입니다.") @RequestParam int page) {
+	public ResponseEntity<ResultResponse> getPostPage(@PathVariable("username") String username,
+		@RequestParam int page) {
 		final Page<MemberPostDTO> postPage = memberPostService.getMemberPostDTOs(username, 3, page);
 
 		return ResponseEntity.ok(ResultResponse.of(ResultCode.FIND_MEMBER_POSTS_SUCCESS, postPage));
@@ -60,7 +56,7 @@ public class MemberPostController {
 
 	// ============== 저장 ================
 	@ApiOperation(value = "멤버 저장한 게시물 15개 조회")
-	@GetMapping("/accounts/{username}/posts/saved/recent")
+	@GetMapping("/accounts/posts/saved/recent")
 	public ResponseEntity<ResultResponse> getRecent1SavedPosts() {
 		final List<MemberPostDTO> postList = memberPostService.getRecent15SavedPostDTOs();
 
@@ -68,10 +64,9 @@ public class MemberPostController {
 	}
 
 	@ApiOperation(value = "멤버 저장한 게시물 페이징 조회(무한스크롤)")
-	@GetMapping("/accounts/{username}/posts/saved")
+	@GetMapping("/accounts/posts/saved")
 	@ApiImplicitParam(name = "page", value = "페이지", required = true, example = "1")
-	public ResponseEntity<ResultResponse> getSavedPostPage(
-		@Validated @NotNull(message = "조회할 게시물 page는 필수입니다.") @RequestParam int page) {
+	public ResponseEntity<ResultResponse> getSavedPostPage(@RequestParam int page) {
 		final Page<MemberPostDTO> postPage = memberPostService.getMemberSavedPostDTOs(3, page);
 
 		return ResponseEntity.ok(ResultResponse.of(ResultCode.FIND_MEMBER_SAVED_POSTS_SUCCESS, postPage));
@@ -84,8 +79,7 @@ public class MemberPostController {
 		@ApiImplicitParam(name = "username", value = "유저네임", required = true, example = "dlwlrma")
 	})
 	@GetMapping("/accounts/{username}/posts/tagged/recent")
-	public ResponseEntity<ResultResponse> getRecent10TaggedPosts(
-		@PathVariable("username") @Validated @NotBlank(message = "username은 필수입니다") String username) {
+	public ResponseEntity<ResultResponse> getRecent10TaggedPosts(@PathVariable("username") String username) {
 		final List<MemberPostDTO> postList = memberPostService.getRecent15TaggedPostDTOs(username);
 
 		return ResponseEntity.ok(ResultResponse.of(ResultCode.FIND_RECENT15_MEMBER_TAGGED_POSTS_SUCCESS, postList));
@@ -98,11 +92,11 @@ public class MemberPostController {
 		@ApiImplicitParam(name = "page", value = "페이지", required = true, example = "1")
 	})
 	@GetMapping("/accounts/{username}/posts/tagged")
-	public ResponseEntity<ResultResponse> getTaggedPostPage(
-		@PathVariable("username") @Validated @NotBlank(message = "username은 필수입니다") String username,
-		@Validated @NotNull(message = "조회할 게시물 page는 필수입니다.") @RequestParam int page) {
+	public ResponseEntity<ResultResponse> getTaggedPostPage(@PathVariable("username") String username,
+		@RequestParam int page) {
 		final Page<MemberPostDTO> postPage = memberPostService.getMemberTaggedPostDTOs(username, 3, page);
 
 		return ResponseEntity.ok(ResultResponse.of(ResultCode.FIND_MEMBER_TAGGED_POSTS_SUCCESS, postPage));
 	}
+
 }
