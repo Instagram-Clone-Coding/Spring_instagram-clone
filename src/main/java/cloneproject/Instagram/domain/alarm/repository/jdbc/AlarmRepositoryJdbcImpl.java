247b24da7b0bd39a09924c1aeec9d819a3c1f3fb
seonpilKim <ksp970306@gmail.com>
2022-06-22 16:18:40 +0900

    Style: 컨벤션 적용 및 메소드명 수정
    Resolve: #166

--- src/main/java/cloneproject/Instagram/domain/alarm/repository/jdbc/AlarmRepositoryJdbcImpl.java
+++ src/main/java/cloneproject/Instagram/domain/alarm/repository/jdbc/AlarmRepositoryJdbcImpl.java
@@ -5,6 +5,7 @@ import cloneproject.Instagram.domain.feed.entity.Comment;
 import cloneproject.Instagram.domain.feed.entity.Post;
 import cloneproject.Instagram.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -16,56 +17,59 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AlarmRepositoryJdbcImpl implements AlarmRepositoryJdbc {
 
-    private final JdbcTemplate jdbcTemplate;
+	private final JdbcTemplate jdbcTemplate;
 
-    @Override
-    public void saveMentionPostAlarms(Member agent, List<Member> targets, Post post, LocalDateTime now) {
-        final String sql =
-                "INSERT INTO alarms (`alarm_created_date`, `alarm_type`, `alarm_agent_id`, `post_id`, `alarm_target_id`) " +
-                        "VALUES(?, ?, ?, ?, ?)";
+	@Override
+	public void saveMentionPostAlarms(Member agent, List<Member> targets, Post post, LocalDateTime now) {
+		final String sql =
+			"INSERT INTO alarms (`alarm_created_date`, `alarm_type`, `alarm_agent_id`, `post_id`, `alarm_target_id`) " +
+				"VALUES(?, ?, ?, ?, ?)";
 
-        jdbcTemplate.batchUpdate(
-                sql,
-                new BatchPreparedStatementSetter() {
-                    @Override
-                    public void setValues(PreparedStatement ps, int i) throws SQLException {
-                        ps.setString(1, now.toString());
-                        ps.setString(2, AlarmType.MENTION_POST.name());
-                        ps.setString(3, agent.getId().toString());
-                        ps.setString(4, post.getId().toString());
-                        ps.setString(5, targets.get(i).getId().toString());
-                    }
+		jdbcTemplate.batchUpdate(
+			sql,
+			new BatchPreparedStatementSetter() {
+				@Override
+				public void setValues(PreparedStatement ps, int i) throws SQLException {
+					ps.setString(1, now.toString());
+					ps.setString(2, AlarmType.MENTION_POST.name());
+					ps.setString(3, agent.getId().toString());
+					ps.setString(4, post.getId().toString());
+					ps.setString(5, targets.get(i).getId().toString());
+				}
 
-                    @Override
-                    public int getBatchSize() {
-                        return targets.size();
-                    }
-                });
-    }
+				@Override
+				public int getBatchSize() {
+					return targets.size();
+				}
+			});
+	}
 
-    @Override
-    public void saveMentionCommentAlarms(Member agent, List<Member> targets, Post post, Comment comment, LocalDateTime now) {
-        final String sql =
-                "INSERT INTO alarms (`alarm_created_date`, `alarm_type`, `alarm_agent_id`, `comment_id`, `post_id`, `alarm_target_id`) " +
-                        "VALUES(?, ?, ?, ?, ?, ?)";
+	@Override
+	public void saveMentionCommentAlarms(Member agent, List<Member> targets, Post post, Comment comment,
+		LocalDateTime now) {
+		final String sql =
+			"INSERT INTO alarms (`alarm_created_date`, `alarm_type`, `alarm_agent_id`, `comment_id`, `post_id`, `alarm_target_id`) "
+				+
+				"VALUES(?, ?, ?, ?, ?, ?)";
 
-        jdbcTemplate.batchUpdate(
-                sql,
-                new BatchPreparedStatementSetter() {
-                    @Override
-                    public void setValues(PreparedStatement ps, int i) throws SQLException {
-                        ps.setString(1, now.toString());
-                        ps.setString(2, AlarmType.MENTION_COMMENT.name());
-                        ps.setString(3, agent.getId().toString());
-                        ps.setString(4, comment.getId().toString());
-                        ps.setString(5, post.getId().toString());
-                        ps.setString(6, targets.get(i).getId().toString());
-                    }
+		jdbcTemplate.batchUpdate(
+			sql,
+			new BatchPreparedStatementSetter() {
+				@Override
+				public void setValues(PreparedStatement ps, int i) throws SQLException {
+					ps.setString(1, now.toString());
+					ps.setString(2, AlarmType.MENTION_COMMENT.name());
+					ps.setString(3, agent.getId().toString());
+					ps.setString(4, comment.getId().toString());
+					ps.setString(5, post.getId().toString());
+					ps.setString(6, targets.get(i).getId().toString());
+				}
 
-                    @Override
-                    public int getBatchSize() {
-                        return targets.size();
-                    }
-                });
-    }
+				@Override
+				public int getBatchSize() {
+					return targets.size();
+				}
+			});
+	}
+
 }
