b1588cc2b8ce8cd42aad1a668b9f1c0fe4642c03
DH CHOI <bluetifulc@gmail.com>
2022-07-29 15:05:26 +0900

    Style: merge conflict 해결 후 관련 파일 import order 정렬

--- src/main/java/cloneproject/Instagram/domain/member/repository/querydsl/MemberPostRepositoryQuerydslImpl.java
+++ src/main/java/cloneproject/Instagram/domain/member/repository/querydsl/MemberPostRepositoryQuerydslImpl.java
@@ -1,25 +1,25 @@
 package cloneproject.Instagram.domain.member.repository.querydsl;
 
-import java.util.List;
+import static cloneproject.Instagram.domain.feed.entity.QBookmark.*;
+import static cloneproject.Instagram.domain.feed.entity.QPost.*;
+import static cloneproject.Instagram.domain.feed.entity.QPostLike.*;
+import static cloneproject.Instagram.domain.feed.entity.QPostTag.*;
 
-import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.JPAExpressions;
-import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
-import cloneproject.Instagram.domain.feed.dto.MemberPostDto;
-import cloneproject.Instagram.domain.feed.dto.QMemberPostDto;
-import cloneproject.Instagram.domain.member.entity.Member;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
 
-import static cloneproject.Instagram.domain.feed.entity.QPost.post;
-import static cloneproject.Instagram.domain.feed.entity.QBookmark.bookmark;
-import static cloneproject.Instagram.domain.feed.entity.QPostLike.*;
-import static cloneproject.Instagram.domain.feed.entity.QPostTag.postTag;
+import cloneproject.Instagram.domain.feed.dto.MemberPostDto;
+import cloneproject.Instagram.domain.feed.dto.QMemberPostDto;
+import cloneproject.Instagram.domain.member.entity.Member;
 
 @RequiredArgsConstructor
 public class MemberPostRepositoryQuerydslImpl implements MemberPostRepositoryQuerydsl {
