d90864da8a20545e3fbe2e729339d4ca240a1bf2
seonpilKim <ksp970306@gmail.com>
2022-08-09 19:45:51 +0900

    Fix: Request @Setter 추가
    @ModelAttribute는 dto에 setter 혹은 생성자가 있으면 HTTP 파라미터를 Java 객체로 변환할 수 있지만, dto의 필드 중 List가 있으면 setter가 없으면 데이터가 제대로 매핑되지 않는 문제가 발생함

--- src/main/java/cloneproject/Instagram/domain/feed/dto/PostUploadRequest.java
+++ src/main/java/cloneproject/Instagram/domain/feed/dto/PostUploadRequest.java
@@ -14,8 +14,10 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostUploadRequest {
