36911f98b23f8c2c256bf92db4fdf3c9b3b3ff14
DH CHOI <bluetifulc@gmail.com>
2022-09-03 17:14:06 +0900

    Test: ResetPasswordCodeRedisRepository 테스트 작성

---
+++ src/test/java/cloneproject/Instagram/domain/member/repository/redis/ResetPasswordCodeRedisRepositoryTest.java
@@ -0,0 +1,55 @@
+package cloneproject.Instagram.domain.member.repository.redis;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
+import org.springframework.context.annotation.Import;
+
+import cloneproject.Instagram.domain.member.entity.redis.ResetPasswordCode;
+import cloneproject.Instagram.global.config.EmbeddedRedisConfig;
+import cloneproject.Instagram.global.config.RedisConfig;
+import cloneproject.Instagram.util.domain.member.redis.ResetPasswordCodeUtils;
+
+@DataRedisTest
+@Import({EmbeddedRedisConfig.class, RedisConfig.class})
+public class ResetPasswordCodeRedisRepositoryTest {
+
+	@Autowired
+	private ResetPasswordCodeRedisRepository resetPasswordCodeRedisRepository;
+
+	@Nested
+	class FindByUsername {
+
+		@Test
+		void resetPasswordCodeExist_ReturnResetPasswordCode() {
+			// given
+			final ResetPasswordCode resetPasswordCode = ResetPasswordCodeUtils.newInstance();
+			resetPasswordCodeRedisRepository.save(resetPasswordCode);
+
+			// when
+			final boolean isPresent = resetPasswordCodeRedisRepository.findByUsername(resetPasswordCode.getUsername())
+				.isPresent();
+
+			// then
+			assertThat(isPresent).isTrue();
+		}
+
+		@Test
+		void resetPasswordCodeNotExist_ReturnEmpty() {
+			// given
+			final ResetPasswordCode resetPasswordCode = ResetPasswordCodeUtils.newInstance();
+
+			// when
+			final boolean isEmpty = resetPasswordCodeRedisRepository.findByUsername(resetPasswordCode.getUsername())
+				.isEmpty();
+
+			// then
+			assertThat(isEmpty).isTrue();
+		}
+
+	}
+
+}
