a85a59aa73fee6504249f894930f444139142478
DH CHOI <bluetifulc@gmail.com>
2022-10-25 19:44:56 +0900

    Feat: AsyncConfig 설정
    - AsyncConfig을 작성해 비동기 메서드가 정상적으로 처리되도록 수정

---
+++ src/main/java/cloneproject/Instagram/global/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package cloneproject.Instagram.global.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig extends AsyncConfigurerSupport {
+
+	@Override
+	public Executor getAsyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(5);
+		executor.setMaxPoolSize(50);
+		executor.setQueueCapacity(50);
+		executor.initialize();
+		return executor;
+	}
+
+}
