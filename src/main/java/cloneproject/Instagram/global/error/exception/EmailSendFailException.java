0a3faa31fc2999dc9ff2d7d404a35616fdad14d4
DH CHOI <bluetifulc@gmail.com>
2022-10-27 22:57:46 +0900

    Style: 빈줄 추가

--- src/main/java/cloneproject/Instagram/global/error/exception/EmailSendFailException.java
+++ src/main/java/cloneproject/Instagram/global/error/exception/EmailSendFailException.java
@@ -3,6 +3,7 @@ package cloneproject.Instagram.global.error.exception;
 import cloneproject.Instagram.global.error.ErrorCode;
 
 public class EmailSendFailException extends BusinessException {
+
 	public EmailSendFailException() {
 		super(ErrorCode.EMAIL_SEND_FAIL);
 	}
