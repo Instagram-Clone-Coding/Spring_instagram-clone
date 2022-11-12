6cfae4738d77d93efc7a4441025757d822434df0
DH CHOI <bluetifulc@gmail.com>
2022-11-12 15:08:51 +0900

    Test: CustomUserDetailsService 테스트 작성

---
+++ src/test/java/cloneproject/Instagram/domain/member/service/CustomUserDetailsServiceTest.java
@@ -0,0 +1,72 @@
+package cloneproject.Instagram.domain.member.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Optional;
+
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import cloneproject.Instagram.domain.member.entity.Member;
+import cloneproject.Instagram.domain.member.repository.MemberRepository;
+import cloneproject.Instagram.util.domain.member.MemberUtils;
+
+@ExtendWith(MockitoExtension.class)
+public class CustomUserDetailsServiceTest {
+
+	private final String MEMBER_ID_VARIABLE_NAME = "id";
+
+	@InjectMocks
+	private CustomUserDetailsService customUserDetailsService;
+
+	@Mock
+	private MemberRepository memberRepository;
+
+	@Nested
+	class LoadUserByUsername {
+
+		@Test
+		void memberExist_ReturnUserDetails() {
+			// given
+			final long memberId = 1L;
+			final Member member = MemberUtils.newInstance();
+			final GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getRole().toString());
+			ReflectionTestUtils.setField(member, MEMBER_ID_VARIABLE_NAME, memberId);
+
+			given(memberRepository.findByUsername(member.getUsername())).willReturn(Optional.of(member));
+
+			// when
+			final UserDetails userDetails = customUserDetailsService.loadUserByUsername(member.getUsername());
+
+			// then
+			assertThat(userDetails.getUsername()).isEqualTo(String.valueOf(memberId));
+			assertThat(userDetails.getPassword()).isEqualTo(String.valueOf(member.getPassword()));
+		}
+
+		@Test
+		void memberNotExist_ThrowException() {
+			// given
+			final String username = MemberUtils.getRandomUsername();
+
+			// when
+			final ThrowableAssert.ThrowingCallable executable = () -> customUserDetailsService.loadUserByUsername(
+				username);
+
+			// then
+			assertThatThrownBy(executable).isInstanceOf(UsernameNotFoundException.class);
+		}
+
+	}
+
+}
